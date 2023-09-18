3742a0b4899747369e9787630982b3a887f5e3fa
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-09-18 13:02:11 -0500

    fdasafdsfdsa

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -2,16 +2,19 @@ import java.util.LinkedList;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
-*/
-public class ListUtil
-{
+ */
+public class ListUtil {
     /**
      * Reverses the elements in a linked list
      *
      * @param strings the linked list to reverse
-    */
-    public static void reverse(LinkedList<String> strings)
-    {
-        ...
+     */
+    public static void reverse(LinkedList<String> strings) {
+        String temp;
+        for (int i = 0; i < (strings.size() / 2); i++) {
+            temp = strings.get(i);
+            strings.set(i, strings.get(strings.size() - i - 1));
+            strings.set(strings.size() - i - 1, temp);
+        }
     }
 }
--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,43 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Diana";)
+        staff.addLast("Harry";)
+        staff.addLast("Romeo";)
+        staff.addLast("Tim";)
+
+        ListIterator<String> iterator = staff.listIterator();
+
+        Iterator.next();
+
+        String name = iterator.next()
+        System.out.println(name)
+        System.out.print("Expected: Harry")
+
+        iterator.add("Juliet")
+        iteratorr.add("Nina")
+
+        iterator.next();
+        iterator.remove();
+
+        System.out.println(staff);
+        System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
+
+        iterator.previous();
+        iterator.set("Albert");
+
+        iterator = staf.listIterator();
+
+        while (Iteerator.hasNext()) {
+            String n = iterator.next()
+            if (n.equals("Juliet"))
+                iterator.remove();
+        }
+
+        for (String n: staff)
+            System.out.print(n + " "));
+        
+        System.out.println("Expected: Diana Harry Albert Tom")
     }
 }
