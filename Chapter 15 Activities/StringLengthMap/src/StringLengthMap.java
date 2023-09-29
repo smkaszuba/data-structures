948f86e290a636ed8e91dd5767c17f182d4760bd
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-09-29 12:52:55 -0500

    fdsafsafdafdssadffasdafsd

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 /**
  * Read all words from a file and add them to a
  * map whose keys are word lengths and whose values
@@ -8,46 +9,43 @@ import java.io.*;
  * the length of their entries.
  * Modify Worked Example 15.1.
  */
-public class StringLengthMap
-{
-    public static void main(String[] args) throws FileNotFoundException
-    {
+public class StringLengthMap {
+    public static void main(String[] args) throws FileNotFoundException {
         String filename = "src/test1.txt";
 
-        try (Scanner in = new Scanner(new File(filename)))
-        {
+        try (Scanner in = new Scanner(new File(filename))) {
 
             // Create your map here
-            
+            Map<Integer, String> lengthMap = new HashMap<>();
 
-            while (in.hasNext())
-            {
+            while (in.hasNext()) {
                 String word = clean(in.next());
                 Integer len = word.length();
 
                 // Update the map here
                 // Modify Worked Example 15.1
-                
-
+                if (lengthMap.containsKey(len))
+                    lengthMap.put(len, lengthMap.get(len) + "," + word);
+                else
+                    lengthMap.put(len, word);
 
             }
 
             // Print the strings, in increasing order of their length
             // Use this format: 1: i, a, i
-        } catch (FileNotFoundException e)
-        {
+            for (Integer len : lengthMap.keySet())
+                System.out.println(len + ": " + lengthMap.get(len));
+
+        } catch (FileNotFoundException e) {
             System.out.println("Cannot open: " + filename);
         }
     }
 
-    public static String clean(String s)
-    {
+    public static String clean(String s) {
         String r = "";
-        for (int i = 0; i < s.length(); i++)
-        {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (Character.isLetter(c))
-            {
+            if (Character.isLetter(c)) {
                 r = r + c;
             }
         }
