33c0003a9552536835f402e4603c5c534d93d5ab
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-10-19 13:07:44 -0500

    fsadsdafsd

--- Chapter 16 Activities/Contains/src/LinkedList.java
+++ Chapter 16 Activities/Contains/src/LinkedList.java
@@ -1,24 +1,23 @@
 import java.util.NoSuchElementException;
+import java.util.ListIterator;
 
 /**
-    A linked list is a sequence of nodes with efficient
-    element insertion and removal. This class 
-    contains a subset of the methods of the standard
-    java.util.LinkedList class.
-*/
-public class LinkedList
-{
+ * A linked list is a sequence of nodes with efficient
+ * element insertion and removal. This class
+ * contains a subset of the methods of the standard
+ * java.util.LinkedList class.
+ */
+public class LinkedList {
     private Node first;
     private int currentSize;
 
     private Node last;
     private int lastIndex;
 
-    /** 
-        Constructs an empty linked list.
-    */
-    public LinkedList()
-    {
+    /**
+     * Constructs an empty linked list.
+     */
+    public LinkedList() {
         first = null;
         currentSize = 0;
         last = null;
@@ -26,41 +25,47 @@ public class LinkedList
     }
 
     /**
-        Computes the size of the linked list
-        @return the number of elements in the list
-    */
-    public int size()
-    {
+     * Computes the size of the linked list
+     * 
+     * @return the number of elements in the list
+     */
+    public int size() {
         return currentSize;
     }
 
     /**
-        Checks if this linked list contains the given object.
-        @param obj The object to be checked for.
-        @return If the object exists in the list.
-    */
-    public boolean contains(Object obj)
-    {
-        // ...
+     * Checks if this linked list contains the given object.
+     * 
+     * @param obj The object to be checked for.
+     * @return If the object exists in the list.
+     */
+    public boolean contains(Object obj) {
+        LinkedListIterator iterator = new LinkedListIterator();
+
+        while (iterator.hasNext())
+            if (iterator.next() == obj)
+                return true;
+
+        return false;
     }
 
     /**
-        Returns the first element in the linked list.
-        @return the first element in the linked list
-    */
-    public Object getFirst()
-    {
+     * Returns the first element in the linked list.
+     * 
+     * @return the first element in the linked list
+     */
+    public Object getFirst() {
         if (first == null)
             throw new NoSuchElementException();
         return first.data;
     }
 
     /**
-        Removes the first element in the linked list.
-        @return the removed element
-    */
-    public Object removeFirst()
-    {
+     * Removes the first element in the linked list.
+     * 
+     * @return the removed element
+     */
+    public Object removeFirst() {
         if (first == null)
             throw new NoSuchElementException();
         Object element = first.data;
@@ -74,11 +79,11 @@ public class LinkedList
     }
 
     /**
-        Adds an element to the front of the linked list.
-        @param element the element to add
-    */
-    public void addFirst(Object element)
-    {
+     * Adds an element to the front of the linked list.
+     * 
+     * @param element the element to add
+     */
+    public void addFirst(Object element) {
         Node newNode = new Node();
         newNode.data = element;
         newNode.next = first;
@@ -90,19 +95,16 @@ public class LinkedList
     }
 
     /**
-        Gets the object at the given index.
-        @param n The index of the object.
-        @return The object at the given index.
-    */
-    public Object get(int n)
-    {
-        if ((lastIndex == -1) || (lastIndex >= n))
-        {
+     * Gets the object at the given index.
+     * 
+     * @param n The index of the object.
+     * @return The object at the given index.
+     */
+    public Object get(int n) {
+        if ((lastIndex == -1) || (lastIndex >= n)) {
             last = getNode(first, n);
             lastIndex = n;
-        }
-        else
-        {
+        } else {
             last = getNode(last, n - lastIndex);
             lastIndex = n;
         }
@@ -110,65 +112,62 @@ public class LinkedList
     }
 
     /**
-        Sets the object at the given index.
-        @param n The index of the object to be set.
-        @param newElement The object to set.
-    */
-    public void set(int n, Object newElement)
-    {
+     * Sets the object at the given index.
+     * 
+     * @param n          The index of the object to be set.
+     * @param newElement The object to set.
+     */
+    public void set(int n, Object newElement) {
         Node node = getNode(first, n);
         node.data = newElement;
     }
 
     /**
-        Gets the node at the given index.
-        @param start The start node to count from.
-        @param distance The index of the object to get.
-        @return The node at the given object.
-    */
-    private static Node getNode(Node start, int distance)
-    {
+     * Gets the node at the given index.
+     * 
+     * @param start    The start node to count from.
+     * @param distance The index of the object to get.
+     * @return The node at the given object.
+     */
+    private static Node getNode(Node start, int distance) {
         if ((distance <= 0) || (start == null))
             return start;
         return getNode(start.next, distance - 1);
     }
 
     /**
-        Returns an iterator for iterating through this list.
-        @return an iterator for iterating through this list
-    */
-    public ListIterator listIterator()
-    {
+     * Returns an iterator for iterating through this list.
+     * 
+     * @return an iterator for iterating through this list
+     */
+    public ListIterator listIterator() {
         return new LinkedListIterator();
     }
 
-    private class Node
-    {
+    private class Node {
         public Object data;
         public Node next;
     }
 
-    private class LinkedListIterator implements ListIterator
-    {
+    private class LinkedListIterator implements ListIterator {
         private Node position;
         private Node previous;
 
         /**
-            Constructs an iterator that points to the front
-            of the linked list.
-        */
-        public LinkedListIterator()
-        {
+         * Constructs an iterator that points to the front
+         * of the linked list.
+         */
+        public LinkedListIterator() {
             position = null;
             previous = null;
         }
 
         /**
-            Moves the iterator past the next element.
-            @return the traversed element
-        */
-        public Object next()
-        {
+         * Moves the iterator past the next element.
+         * 
+         * @return the traversed element
+         */
+        public Object next() {
             if (!hasNext())
                 throw new NoSuchElementException();
             previous = position; // Remember for remove
@@ -182,13 +181,13 @@ public class LinkedList
         }
 
         /**
-            Tests if there is an element after the iterator 
-            position.
-            @return true if there is an element after the iterator 
-            position
-        */
-        public boolean hasNext()
-        {
+         * Tests if there is an element after the iterator
+         * position.
+         * 
+         * @return true if there is an element after the iterator
+         *         position
+         */
+        public boolean hasNext() {
             if (position == null)
                 return first != null;
             else
@@ -196,19 +195,16 @@ public class LinkedList
         }
 
         /**
-            Adds an element before the iterator position
-            and moves the iterator past the inserted element.
-            @param element the element to add
-        */
-        public void add(Object element)
-        {
-            if (position == null)
-            {
+         * Adds an element before the iterator position
+         * and moves the iterator past the inserted element.
+         * 
+         * @param element the element to add
+         */
+        public void add(Object element) {
+            if (position == null) {
                 addFirst(element);
                 position = first;
-            }
-            else
-            {
+            } else {
                 Node newNode = new Node();
                 newNode.data = element;
                 newNode.next = position.next;
@@ -220,20 +216,16 @@ public class LinkedList
         }
 
         /**
-            Removes the last traversed element. This method may
-            only be called after a call to the next() method.
-        */
-        public void remove()
-        {
+         * Removes the last traversed element. This method may
+         * only be called after a call to the next() method.
+         */
+        public void remove() {
             if (previous == position)
                 throw new IllegalStateException();
 
-            if (position == first)
-            {
+            if (position == first) {
                 removeFirst();
-            }
-            else
-            {
+            } else {
                 previous.next = position.next;
                 currentSize--;
             }
@@ -241,12 +233,12 @@ public class LinkedList
         }
 
         /**
-            Sets the last traversed element to a different 
-            value. 
-            @param element the element to set
-        */
-        public void set(Object element)
-        {
+         * Sets the last traversed element to a different
+         * value.
+         * 
+         * @param element the element to set
+         */
+        public void set(Object element) {
             if (position == null)
                 throw new NoSuchElementException();
             position.data = element;
